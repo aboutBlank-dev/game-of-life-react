c7f1c8e87d0dec553330df5d894b8f1ac99dd3e6
aboutBlank-dev <josecolaco1999@gmail.com>
2023-12-08 22:24:58 +0000

    working simulation for game of life

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Canvas from "./components/GridCanvas";
 
 type Props = {};
+
+let timeoutHandler: number | null = null;
 
 export type GridCell = {
   x: number;
@@ -9,7 +11,38 @@ export type GridCell = {
   alive: boolean;
 };
 
-function generateInitialGrid(gridSize: number) {
+function calculateLiveNeighbors(gridCells: GridCell[][], x: number, y: number) {
+  let liveNeighbors = 0;
+  const dirs = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+  ];
+  for (let i = 0; i < dirs.length; i++) {
+    const dir = dirs[i];
+    let y1 = y + dir[0];
+    let x1 = x + dir[1];
+
+    if (
+      x1 >= 0 &&
+      y1 >= 0 &&
+      x1 < gridCells.length &&
+      y1 < gridCells.length &&
+      gridCells[y1][x1].alive
+    ) {
+      liveNeighbors++;
+    }
+  }
+
+  return liveNeighbors;
+}
+
+function generateEmptyGrid(gridSize: number) {
   const newGrid: GridCell[][] = [];
   for (let i = 0; i < gridSize; i++) {
     newGrid[i] = [];
@@ -17,21 +50,72 @@ function generateInitialGrid(gridSize: number) {
       newGrid[i][j] = {
         x: i,
         y: j,
-        alive: Math.random() > 0.9,
+        alive: false,
       };
     }
   }
   return newGrid;
 }
 
+function generateInitialGrid(gridSize: number) {
+  const newGrid: GridCell[][] = generateEmptyGrid(gridSize);
+  for (let y = 0; y < newGrid.length; y++) {
+    for (let x = 0; x < newGrid[y].length; x++) {
+      if (Math.random() < 0.5) {
+        newGrid[y][x].alive = Math.random() < 0.5;
+      }
+    }
+  }
+  return newGrid;
+}
+
 function Game({}: Props) {
+  const [isRunning, setIsRunning] = useState(false);
+  const [simulationDelay, setSimulationDelay] = useState(100);
   const [gridCells, setGridCells] = useState<GridCell[][]>(
     generateInitialGrid(20)
   );
 
+  const runSimulation = () => {
+    if (!isRunning) {
+      return;
+    }
+
+    const newGrid = generateEmptyGrid(gridCells.length);
+    for (let y = 0; y < gridCells.length; y++) {
+      for (let x = 0; x < gridCells[y].length; x++) {
+        const cell = gridCells[y][x];
+        const liveNeighbors = calculateLiveNeighbors(gridCells, x, y);
+
+        if (cell.alive && (liveNeighbors === 2 || liveNeighbors === 3)) {
+          newGrid[y][x].alive = true;
+        } else if (!cell.alive && liveNeighbors === 3) {
+          newGrid[y][x].alive = true;
+        }
+      }
+    }
+    setGridCells(newGrid);
+  };
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const interval = setInterval(runSimulation, simulationDelay);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <div className='w-full h-screen'>
       <Canvas gridCells={gridCells} />
+      <button
+        onClick={() => {
+          setIsRunning(!isRunning);
+        }}
+      >
+        TOGGLE
+      </button>
     </div>
   );
 }
