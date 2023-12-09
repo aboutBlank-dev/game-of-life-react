f169579db100c465a0baae2dd2691586ea7a10ed
aboutBlank-dev <josecolaco1999@gmail.com>
2023-12-09 02:15:59 +0000

    added buttons to control the game

--- src/Game.tsx
+++ src/Game.tsx
@@ -112,16 +112,49 @@ function Game({}: Props) {
     newGrid[y][x].alive = !newGrid[y][x].alive;
     setGridCells(newGrid);
   };
+
+  const toggleSimulation = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const clearGrid = () => {
+    setGridCells(generateEmptyGrid(gridCells.length));
+    setIsRunning(false);
+  };
+
+  const randomGrid = () => {
+    setGridCells(generateInitialGrid(gridCells.length));
+    setIsRunning(false);
+  };
+
+  const startStopStyle = `${isRunning ? "bg-red-500" : "bg-green-500"} ${
+    isRunning ? "hover:bg-red-400" : "hover:bg-green-400"
+  } ${isRunning ? "border-red-700" : "border-green-700"} ${
+    isRunning ? "hover:border-red-500" : "hover:border-green-500"
+  }`;
   return (
     <div className='w-full h-screen'>
       <Canvas gridCells={gridCells} onCellClicked={onCellClicked} />
-      <button
-        onClick={() => {
-          setIsRunning(!isRunning);
-        }}
-      >
-        TOGGLE
-      </button>
+      <div className='flex flex-row space-x-8 w-full p-4'>
+        <button
+          onClick={toggleSimulation}
+          className={`text-white font-bold py-2 px-4 border-b-4 rounded ${startStopStyle}`}
+        >
+          Start/Stop
+        </button>
+        <button
+          onClick={clearGrid}
+          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+        >
+          Clear Grid
+        </button>
+        <button
+          onClick={randomGrid}
+          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+        >
+          Random Grid
+        </button>
+      </div>
     </div>
   );
 }
